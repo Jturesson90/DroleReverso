5418dbee677b62a01692a269ae8cfd7a0005ddcd
Jturesson90 <Jturesson90@gmail.com>
2016-03-15 00:16:22 +0100

    2 Boards and Adapter is Clearing the content waiting for filling it with children

--- Reverso/Assets/ThemesAdapter.cs
+++ Reverso/Assets/ThemesAdapter.cs
@@ -1,18 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class ThemesAdapter : MonoBehaviour
 {
-
+    public GameObject ThemeRowPrefab;
+    public List<ThemeRow> themeRows;
 
     // Use this for initialization
     void Start()
     {
-        //ResetAllToggles();
-        PlayerPrefs.SetInt("RADIO_BUTTON0", 0);
+       // ResetAllToggles();
+        //PlayerPrefs.SetInt("RADIO_BUTTON0", 0);
+        //DeleteAllChildren();
     }
-
+    void DeleteAllChildren()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Transform child = transform.GetChild(i);
+            Destroy(child.gameObject, 0f);
+        }
+    }
     // Update is called once per frame
     void Update()
     {

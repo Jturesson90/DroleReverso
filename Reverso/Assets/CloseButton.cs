1e8c49987b7f495a210e0fc225423b1ba960485d
Jesper Turesson <jturesson90@gmail.com>
2016-03-14 14:45:29 +0100

    CloseButtons

---
+++ Reverso/Assets/CloseButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloseButton : MonoBehaviour
+{
+
+    public void OnCloseButtonClicked()
+    {
+        NavigationUtil.OnBackbuttonPressedInMenu();
+    }
+}
